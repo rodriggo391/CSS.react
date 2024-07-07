04345091d86f38aa96db35e9cedee47a3b3401ee
Rodrigo Duran <rodriggo.duran.88@gmail.com>
2024-07-07 14:55:01 -0300

    Create CSS.react.js
    CSS.react

---
+++ CSS.react.js
@@ -0,0 +1,48 @@
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif;
+  margin: 10px;
+  padding: 4;
+  background-color: #333;
+}
+
+x1 {
+  margin-top: 0;
+  font-size: 32px;
+}
+
+y1 {
+  margin-top: 0;
+  font-size: 20px;
+}
+
+h3 {
+  margin-top: 0;
+  font-size: 18px;
+}
+
+h4 {
+  margin-top: 0;
+  font-size: 16px;
+}
+
+h5 {
+  margin-top: 0;
+  font-size: 14px;
+}
+
+h6 {
+  margin-top: 0;
+  font-size: 12px;
+}
+
+code {
+  font-size: 1.2em;
+}
+
+ul {
+  padding-inline-start: 20px;
+}
